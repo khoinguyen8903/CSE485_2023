3debd0e80aa308df38392364bc880f1518514a7f
khoinguyen8903 <127465422+khoinguyen8903@users.noreply.github.com>
2024-10-26 08:08:15 +0700

    Add files via upload

---
+++ EX07/app/src/main/java/com/example/bai07/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bai07;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.btnNext);
+        btn1.setOnClickListener(view -> {
+            Intent intent1 = new Intent(MainActivity.this,ChildActivity.class);
+            startActivity(intent1);
+        });
+    }
+}
